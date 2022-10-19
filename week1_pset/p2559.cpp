82d1fee2764709f4fb27961e8050d6f642970073
key <rlfbd5142@gmail.com>
2022-10-19 21:41:00 +0900

    [week1-p2559] 부분합을 통해서 시간 복잡도 줄이기
    - 범위에서 조심해야됨 <=, < 인지
    - 훈련이 더 필요함

---
+++ week1_pset/p2559.cpp
@@ -0,0 +1,30 @@
+//
+// Created by 남기륭 on 2022/10/19.
+//
+#include <iostream>
+#include <limits>
+
+using namespace std;
+
+int totalSize;
+int windowSize;
+int temperture[1000000];
+int temp;
+
+int main() {
+
+    cin >> totalSize >> windowSize;
+
+    long long max = std::numeric_limits<int>::lowest();
+    for (int i = 1; i <= totalSize; ++i) {
+        cin >> temp;
+        temperture[i] = temperture[i - 1] + temp;
+    }
+
+    for (int i = windowSize; i <= totalSize; ++i) {
+        long long sum = temperture[i] - temperture[i - windowSize];
+        max = std::max(sum, max);
+    }
+
+    cout << max;
+}
