1dcc558d27011e899f9f7bfdabbced5c60dbadfc
Be-The-Key <rlfbd5142@gmail.com>
2022-10-19 19:57:07 +0900

    [Silver III] Title: 한국이 그리울 땐 서버에 접속하지, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

---
+++ 백준/Silver/9996. 한국이 그리울 땐 서버에 접속하지/한국이 그리울 땐 서버에 접속하지.cc
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int inputInt;
+string inputStr;
+vector<string> strArray;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    cin >> inputInt;
+    cin >> inputStr;
+
+    for (int i = 0; i < inputInt; ++i) {
+        string testStr;
+        cin >> testStr;
+        strArray.push_back(testStr);
+    }
+
+    long pos = inputStr.find("*");
+
+    string prefix = inputStr.substr(0, pos);
+    string suffix = inputStr.substr(pos + 1);
+
+    for (const auto &item: strArray) {
+        if (prefix.size() + suffix.size() > item.size()) {
+            cout << "NE" << '\n';
+            continue;
+        }
+
+        if ((prefix == item.substr(0, prefix.size())) && (suffix == item.substr(item.size() - suffix.size()))) {
+            cout << "DA" << '\n';
+        } else {
+            cout << "NE" << '\n';
+        }
+    }
+
+    return 0;
+}
