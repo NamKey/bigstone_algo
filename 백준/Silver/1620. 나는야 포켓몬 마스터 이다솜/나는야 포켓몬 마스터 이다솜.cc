24a8eb86369796264a8b5b28d8a89d9a5259f727
Be-The-Key <rlfbd5142@gmail.com>
2022-10-19 22:12:13 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 200 ms, Memory: 23944 KB -BaekjoonHub

---
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.cc
@@ -0,0 +1,40 @@
+#include "cstdlib"
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+int pocketCount;
+int problemCount;
+
+
+string pocketMon;
+map<string, int> pocketMap;
+map<int, string> pocketMap2;
+string problemName;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    cin >> pocketCount >> problemCount;
+
+    for (int i = 0; i < pocketCount; ++i) {
+        cin >> pocketMon;
+        pocketMap.emplace(pocketMon, i + 1);
+        pocketMap2.emplace(i + 1, pocketMon);
+    }
+
+    for (int i = 0; i < problemCount; ++i) {
+        cin >> problemName;
+
+        if (std::atoi(problemName.c_str()) == 0) {
+            cout << pocketMap[problemName] << '\n';
+        } else {
+            cout << pocketMap2[std::atoi(problemName.c_str())] << '\n';
+        }
+    }
+
+    return 0;
+}
