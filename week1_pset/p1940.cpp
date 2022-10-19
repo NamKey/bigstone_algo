b97638b0c7aed8cb4542d79332748c49a0e72f30
key <rlfbd5142@gmail.com>
2022-10-20 00:12:16 +0900

    [week1-p1940] 조합안하고 풀기

---
+++ week1_pset/p1940.cpp
@@ -0,0 +1,37 @@
+//
+// Created by 남기륭 on 2022/10/19.
+//
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+int totalCount;
+int targetNumber;
+map<int, int> joinMap;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    cin >> totalCount >> targetNumber;
+
+    for (int i = 0; i < totalCount; ++i) {
+        int join;
+        cin >> join;
+        joinMap[join]++;
+    }
+
+    int ret = 0;
+
+    for (const auto &item: joinMap) {
+        if (joinMap.find(targetNumber - item.first)->second == 1) {
+            ret++;
+        }
+    }
+
+    cout << ret / 2 << '\n';
+
+    return 0;
+}
