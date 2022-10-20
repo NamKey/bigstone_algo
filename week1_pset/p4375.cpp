89fc49a106c45fe48c89006363a333cdf53b8142
key <rlfbd5142@gmail.com>
2022-10-20 13:31:13 +0900

    [week1-p4375] 숫자가지고 노는 방식에 대한 고려가 필요함

---
+++ week1_pset/p4375.cpp
@@ -0,0 +1,31 @@
+//
+// Created by 남기륭 on 2022/10/20.
+//
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int inputNum;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    while (scanf("%d", &inputNum) != EOF) {
+        int init = 1;
+        int ret = 1;
+
+        while (true) {
+            if (init % inputNum != 0) {
+                init = init * 10 + 1;
+                init %= inputNum;
+                ret++;
+            } else {
+                cout << ret << '\n';
+                break;
+            }
+        }
+    }
+}
