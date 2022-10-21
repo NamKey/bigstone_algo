c5ce529f4a0a80e69d84c7239c3d06bfb6caf839
Be-The-Key <rlfbd5142@gmail.com>
2022-10-21 16:50:00 +0900

    [Silver V] Title: 기상캐스터, Time: 0 ms, Memory: 2076 KB -BaekjoonHub

---
+++ 백준/Silver/10709. 기상캐스터/기상캐스터.cc
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <utility>
+
+using namespace std;
+
+int H;
+int W;
+
+char cloudMap[104][104];
+int cloudDisMap[104][104];
+
+int main() {
+    cin >> H >> W;
+
+    for (int i = 0; i < H; ++i) {
+        string line;
+        cin >> line;
+        for (int j = 0; j < W; ++j) {
+            cloudMap[i][j] = line[j];
+
+            if (cloudMap[i][j] == 'c') {
+                cloudDisMap[i][j] = 0;
+            } else {
+                cloudDisMap[i][j] = -1;
+            }
+        }
+    }
+
+    for (int i = 0; i < H; ++i) {
+        for (int j = 0; j < W; ++j) {
+            if (cloudDisMap[i][j] == 0) {
+                int cnt = 1;
+                while (cloudDisMap[i][j + 1] == -1) {
+                    cloudDisMap[i][j + 1] = cnt++;
+                    j++;
+                }
+            }
+        }
+    }
+
+    for (int i = 0; i < H; ++i) {
+        for (int j = 0; j < W; ++j) {
+            cout << cloudDisMap[i][j] << " ";
+        }
+
+        cout << '\n';
+    }
+}
