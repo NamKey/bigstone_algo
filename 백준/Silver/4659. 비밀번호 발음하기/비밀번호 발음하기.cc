5de029ecd8d3a13d6d3054bb4a17298b9c393ce7
Be-The-Key <rlfbd5142@gmail.com>
2022-10-21 12:54:33 +0900

    [Silver V] Title: 비밀번호 발음하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/4659. 비밀번호 발음하기/비밀번호 발음하기.cc
@@ -0,0 +1,61 @@
+#include <iostream>
+
+using namespace std;
+
+int moeumCount;
+int jaeumCount;
+
+bool hasMoeum;
+bool isUnacceptSerial;
+bool isTripleSerial;
+bool isGood;
+
+string word;
+
+int main() {
+
+    while (cin >> word) {
+
+        if (word == "end") {
+            break;
+        }
+
+        char prevChar = '\0';
+        char curChar;
+        hasMoeum = false;
+        isTripleSerial = false;
+        isUnacceptSerial = false;
+        jaeumCount = 0;
+        moeumCount = 0;
+
+        for (const auto &item: word) {
+            if (item == 'a' || item == 'e' || item == 'i' || item == 'o' || item == 'u') {
+                hasMoeum = true;
+                moeumCount++;
+                jaeumCount = 0;
+            } else {
+                jaeumCount++;
+                moeumCount = 0;
+            }
+
+            if (jaeumCount == 3 || moeumCount == 3) {
+                isTripleSerial = true;
+            }
+
+            curChar = item;
+            if (curChar == prevChar && (curChar != 'e' && curChar != 'o')) {
+                isUnacceptSerial = true;
+            }
+
+            prevChar = curChar;
+        }
+
+        isGood = hasMoeum && !isTripleSerial & !isUnacceptSerial;
+
+        if (isGood) {
+            cout << "<" << word << ">" << " is acceptable.\n";
+        } else {
+            cout << "<" << word << ">" << " is not acceptable.\n";
+        }
+    }
+}
