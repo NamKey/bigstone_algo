b68b598deb386db41eccd9e552e18550a59a2195
key <rlfbd5142@gmail.com>
2022-10-21 20:18:23 +0900

    [week2-p9012] 풀어봤던거 계속 복습해보기

---
+++ week2_pset/p9012.cpp
@@ -0,0 +1,36 @@
+//
+// Created by 남기륭 on 2022/10/21.
+//
+
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+int N;
+
+int main() {
+
+    cin >> N;
+
+    for (int i = 0; i < N; ++i) {
+        string parenthesis;
+        cin >> parenthesis;
+
+        stack<char> parenthStack;
+        for (int j = 0; j < parenthesis.size(); ++j) {
+            char b = parenthesis[j];
+            if (!parenthStack.empty() && parenthStack.top() == '(' && b == ')') {
+                parenthStack.pop();
+            } else {
+                parenthStack.push(b);
+            }
+        }
+
+        if (parenthStack.empty()) {
+            cout << "YES" << "\n";
+        } else {
+            cout << "NO" << "\n";
+        }
+    }
+}
