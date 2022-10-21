abe0faae19a47ae3b4ccd7a9aadd0159a108795b
key <rlfbd5142@gmail.com>
2022-10-21 23:16:42 +0900

    [week2-p4949] 다른 방식으로도 풀어보기

---
+++ week2_pset/p4949.cpp
@@ -0,0 +1,35 @@
+//
+// Created by 남기륭 on 2022/10/21.
+//
+
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+int main() {
+    string lineStr;
+    while (getline(cin, lineStr)) {
+
+        if (lineStr[0] == '.') break;
+
+        stack<char> parenthesisStack;
+        for (const auto &item: lineStr) {
+            if (isalpha(item) || item == ' ' || item == '.') continue;
+
+            if (!parenthesisStack.empty() && parenthesisStack.top() == '(' && item == ')') {
+                parenthesisStack.pop();
+            } else if (!parenthesisStack.empty() && parenthesisStack.top() == '[' && item == ']') {
+                parenthesisStack.pop();
+            } else {
+                parenthesisStack.push(item);
+            }
+        }
+
+        if (parenthesisStack.empty()) {
+            cout << "yes" << '\n';
+        } else {
+            cout << "no" << '\n';
+        }
+    }
+}
