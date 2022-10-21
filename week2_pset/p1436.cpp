69e088df778b13ac9729ea9fe9f5ac2ddbe14730
key <rlfbd5142@gmail.com>
2022-10-21 19:48:35 +0900

    [week2-p1436] 무식하게 시작하는 것이 가장 첫번째 방식이 되어야 됨

---
+++ week2_pset/p1436.cpp
@@ -0,0 +1,29 @@
+//
+// Created by 남기륭 on 2022/10/21.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int N;
+int lastIndex;
+int ret;
+
+int main() {
+
+    cin >> N;
+    while (true) {
+        if (to_string(ret).find("666") != string::npos) {
+            lastIndex++;
+        }
+
+        if (lastIndex == N) {
+            break;
+        }
+
+        ret++;
+    }
+
+    cout << ret << '\n';
+}
