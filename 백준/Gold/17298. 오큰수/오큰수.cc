01dd1fe587613621465d320b7a92e1a9ddb39d99
Be-The-Key <rlfbd5142@gmail.com>
2022-10-22 19:14:00 +0900

    [Gold IV] Title: 오큰수, Time: 636 ms, Memory: 13996 KB -BaekjoonHub

---
+++ 백준/Gold/17298. 오큰수/오큰수.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+int N;
+int arr[1000004];
+int ret[1000004];
+
+int main() {
+
+    cin >> N;
+
+    stack<int> okenStack;
+
+    fill(&ret[0], &ret[0] + 1000004, -1);
+    for (int i = 0; i < N; ++i) {
+        cin >> arr[i];
+
+        while (!okenStack.empty() && arr[okenStack.top()] < arr[i]) {
+            ret[okenStack.top()] = arr[i];
+            okenStack.pop();
+        }
+
+        okenStack.push(i);
+    }
+
+    for (int i = 0; i < N; ++i) {
+        cout << ret[i] << ' ';
+    }
+}
