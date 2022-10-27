3efb43b74b1121faea07c3f442ffdf80b6cd110d
key <rlfbd5142@gmail.com>
2022-10-27 22:12:51 +0900

    [week3-p16637] 어떻게 이런 아이디어를 떠올리지?

---
+++ week3_pset/p16637.cpp
@@ -0,0 +1,19 @@
+//
+// Created by 남기륭 on 2022/10/27.
+//
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+long long res;
+
+int N;
+
+int main() {
+    cin >> N;
+
+    string input;
+    cin >> input;
+
+}
