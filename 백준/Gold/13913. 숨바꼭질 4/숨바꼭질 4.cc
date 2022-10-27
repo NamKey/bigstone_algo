9094249b4f74ba63ceb9f35d244cc08d21eb73cc
Be-The-Key <rlfbd5142@gmail.com>
2022-10-28 00:10:22 +0900

    [Gold IV] Title: 숨바꼭질 4, Time: 20 ms, Memory: 4368 KB -BaekjoonHub

---
+++ 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.cc
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int visited[200004];
+int trace[200004];
+
+int N, K;
+
+int main() {
+    cin >> N >> K;
+
+    visited[N] = 1;
+    queue<int> queue;
+    queue.push(N);
+
+    while (queue.size()) {
+        int here = queue.front();
+        queue.pop();
+
+        if (here == K) {
+            break;
+        }
+
+        for (int next: {here - 1, here + 1, 2 * here}) {
+            if (next < 0 || next >= 200000 || visited[next]) {
+                continue;
+            }
+
+            visited[next] = visited[here] + 1;
+            queue.push(next);
+            trace[next] = here;
+        }
+    }
+
+    vector<int> traceV;
+    for(int i = K; i != N; i = trace[i]) {
+        traceV.push_back(i);
+    }
+    traceV.push_back(N);
+    reverse(traceV.begin(), traceV.end());
+
+    cout << visited[K] - 1 << '\n';
+
+    for(const auto item: traceV) {
+        cout << item << ' ';
+    }
+}
